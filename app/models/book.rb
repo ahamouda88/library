8768affb50b0e4432e81d1a2eb21b24e9f5dcd7f
ahamouda <ahmed.hamouda8888@gmail.com>
2017-12-07 19:13:26 -0500

    add book controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   belongs_to :subject
 
   validates_presence_of :title
-  validates_numericality_of :price, :message=>"Error Message"
+  validates_numericality_of :price, :message=>"Price should be a number!"
 end
