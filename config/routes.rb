13a2c83eb499dba2c70ae73cb4a49aa5ad071a4c
ahamouda <ahmed.hamouda8888@gmail.com>
2017-12-09 23:38:58 -0500

    fix delete request

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'book', to: 'book#list'
-  get 'book/:id', to: 'book#show'
   get 'book/new'
+  get 'book/:id', to: 'book#show'
   post 'book', to: 'book#create'
   patch 'book', to: 'book#update'
   get 'book/:id/edit', to: 'book#edit'
-  delete 'book/:id', to: 'book#delete'
+  delete 'book', to: 'book#delete'
   get 'book/subjects', to: 'book#show_subjects'
 end
